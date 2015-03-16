8a0e3d5133db43a985106b287b008d1d4d1e10a0
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2015-03-16 10:37:44 -0400

    fixed small bug after dragos upgrades

--- condor_blacklist.sh
+++ condor_blacklist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 condor_q $USER  | grep $USER | awk '{print "condor_q -l "$1" | grep RemoteHost"}'|bash|sed -e 's/^.*@/Machine!="/'>>temporary_blist.txt
 echo "parsing blacklist to keep only 1 copy of each bad machine"
-cat condor_blacklist.txt >> temporary_blacklist.txt
-cat temporary_blacklist.txt | sort | uniq > condor_blacklist.txt
+cat condor_blacklist.txt >> temporary_blist.txt
+cat temporary_blist.txt | sort | uniq > condor_blacklist.txt
 
 rm temporary_blist.txt 
